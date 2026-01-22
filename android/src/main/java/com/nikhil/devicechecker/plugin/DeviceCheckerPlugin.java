de6c34a0b54f6ff020a584687f8c4d54bd5a47dc
Nikhil More <nikhil@customtechnologies.biz>
2026-01-22 11:31:33 +0530

    Add Java plugin and .aar library

--- android/src/main/java/com/nikhil/devicechecker/plugin/DeviceCheckerPlugin.java
+++ android/src/main/java/com/nikhil/devicechecker/plugin/DeviceCheckerPlugin.java
@@ -7,20 +7,33 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 import com.nikhil.devicecheckerlibrary.DeviceChecker;
 
 @CapacitorPlugin(name = "DeviceChecker")
-class DeviceCheckerPlugin : Plugin() {
+public class DeviceCheckerPlugin : Plugin() {
 
     @PluginMethod
-    fun getDeviceDetail(call: PluginCall) {
-        val checker = DeviceChecker(context)  // Use your library
-        val result = checker.getDeviceDetail()
+     @PluginMethod
+    public void getDeviceDetail(PluginCall call) {
+        Context context = getContext();
+        DeviceChecker checker = new DeviceChecker(context);
+        DeviceDetail result = checker.getDeviceDetail();
 
-        // Convert result to JSObject to return to Ionic
-        val ret = JSObject().apply {
-            put("isEmulator", result.isEmulator)
-            put("isRooted", result.isRooted)
-            put("isSecure", result.isSecure)
-        }
+        JSObject ret = new JSObject();
+        ret.put("isEmulator", result.isEmulator());
+        ret.put("isRooted", result.isRooted());
+        ret.put("isSecure", result.isSecure());
 
-        call.resolve(ret)
+        call.resolve(ret);
     }
+    // fun getDeviceDetail(call: PluginCall) {
+    //     val checker = DeviceChecker(context)  // Use your library
+    //     val result = checker.getDeviceDetail()
+
+    //     // Convert result to JSObject to return to Ionic
+    //     val ret = JSObject().apply {
+    //         put("isEmulator", result.isEmulator)
+    //         put("isRooted", result.isRooted)
+    //         put("isSecure", result.isSecure)
+    //     }
+
+    //     call.resolve(ret)
+    // }
 }
